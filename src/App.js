c5e623987d552063a80df8fc85f4cb272cad8871
Natasha Seeto <tashseeto@gmail.com>
2020-11-23 22:14:06 +1000

    Added FE-005-C - helper (Login.jsx), updated App.js with links & App.css (fonts)

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from "./components/nav/Nav";
+import SplashPage from './pages/SplashPage';
+import './App.css';
+
+
+function App() {
+
+  return (
+    
+    <Router>
+
+        <div id="header-nav">
+            <Nav />
+        </div>
+
+        <div>
+        <Switch>
+            <Route path="/">
+                <SplashPage />
+            </Route>    
+        
+        </Switch>
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
