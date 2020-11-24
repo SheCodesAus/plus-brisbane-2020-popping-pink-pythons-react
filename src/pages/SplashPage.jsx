97ee64a4811a5d2910253a3d2e635e9dbf8c3fa4
MV <68259555+MaddieVince@users.noreply.github.com>
2020-11-24 17:28:54 +1000

    Update SplashPage.jsx

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../App.css';
 import SplashImage from '../components/SplashImage/SplashImage';
-import ActionButton from '../components/Button/ActionButton'
 
 function SplashPage() {
     return (
         <div>
         <SplashImage />
-        <ActionButton />
         </div>
     )
 }
