e24487c197a6fb5a3d4d81bae19bb33b8910d10e
gianninapg@gmail.com <Gi@090488>
2020-11-25 21:06:43 +1000

    sticky nav bar created

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import "./Header.css";
+import Logo from "../images/Logo.jpg";
+
+
+  const Navbar=() => {
+    const [scrolled,setScrolled]=React.useState(false);
+  
+    const handleScroll=() => {
+      const offset=window.scrollY;
+      if(offset > 200 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+    useEffect(() => {
+      window.addEventListener('scroll',handleScroll)
+    })
+  
+    let x=['navbar'];
+    if(scrolled){
+      x.push('scrolled');
+    }
+    return (
+      <header className={x.join(" ")}>
+          <div className="logo">
+          <img src={Logo} alt="logo" className="Logo"/>
+          </div>
+  
+      </header>
+    )
+  };
+  
+  export default Navbar;
