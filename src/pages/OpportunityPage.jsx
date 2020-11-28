a020c2b25dd6a336be4b893f8619a6423e25500c
gianninapg@gmail.com <Gi@090488>
2020-11-28 12:46:58 +1000

    updated opportunityPage

--- src/pages/OpportunityPage.jsx
+++ src/pages/OpportunityPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Header from "../components/Header/Header";
 
 function OpportunityPage() {
-    const [opportunityData, setOpportunityData] = useState({ pledges: [] });
+    const [opportunityData, setOpportunityData] = useState();
     const { id } = useParams();
     
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}opportunity/${id}`)
+        fetch(`${process.env.REACT_APP_API_URL}/opportunity/${id}`)
         .then((results) => {
         return results.json();
         })
@@ -17,10 +18,11 @@ function OpportunityPage() {
 
     return (
         <div>
-        <h2>{ projectData .title}</h2>
-        <h3>Created at: { projectData .date_created}</h3>
+        <div className="header"><Header /></div>
+        <h2>{ opportunityData .title}</h2>
+        <h3>Created at: { opportunityData .date_created}</h3>
         <h3> {` Status: $ { opportunityData .is_open} `} </h3>
-        <h3>Pledges:</h3>
+        <h3>Opportunity Objectives:</h3>
     </div>
     );
 }
