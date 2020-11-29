347b55ac8e823be018c163c01dbaab5a6f2a70af
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-11-29 20:58:46 +1000

    added opportunity/:id route to test

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ import SplashPage from "./pages/SplashPage";
 import FeedPage from "./pages/FeedPage";
 import RegisterUserPage from "./pages/RegisterUserPage";
 import NewOpportunityPage from "./pages/NewOpportunityPage";
+import OpportunityPage from "./pages/OpportunityPage";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Switch>
+        <Route path="/opportunity">
+          <FeedPage />
+        </Route>
 
-        <Switch>
-          <Route path="/opportunity">
-            <FeedPage />
-          </Route>
+        <Route path="/opportunity/:id">
+          <OpportunityPage />
+        </Route>
 
-          <Route path="/register">
-            <RegisterUserPage />
-          </Route>
+        <Route path="/register">
+          <RegisterUserPage />
+        </Route>
 
-          <Route path="/login">
-            <LoginPage />
-          </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
 
-          <Route path="/NewOpportunity">
-            <NewOpportunityPage />
-          </Route>
+        <Route path="/NewOpportunity">
+          <NewOpportunityPage />
+        </Route>
 
-          <Route path="/">
-            <SplashPage />
-          </Route>
-        </Switch>
-
+        <Route path="/">
+          <SplashPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
