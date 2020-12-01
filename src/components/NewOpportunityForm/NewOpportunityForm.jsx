f1fbeb7554f77f59b3c051f4b3db0f3c407a342f
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-12-01 20:41:40 +1000

    removing local path to a folder

--- src/components/NewOpportunityForm/NewOpportunityForm.jsx
+++ src/components/NewOpportunityForm/NewOpportunityForm.jsx
@@ -1,118 +1,3 @@
-// import React, { useState } from "react";
-// import { useHistory } from "react-router-dom";
-// import "./NewOpportunityForm.css";
-
-// function NewOpportunityForm() {
-//   const [opportunityData, setOpportunityData] = useState({
-// title: "",
-// location: "",
-// organisation: "",
-// description: "",
-// objectives: "",
-// image: "",
-// start_date: "",
-// close_date: "",
-// amount: "",
-// opp_type: "",
-// opp_link: "",
-//   });
-
-//   const history = useHistory();
-
-//   const handleChange = (e) => {
-//     const { id, value } = e.target;
-//     setOpportunityData((prevOpportunityData) => ({
-//       ...prevOpportunityData,
-//       [id]: value,
-//     }));
-//   };
-
-//   const postData = async () => {
-//     const response = await fetch(
-//       `${process.env.REACT_APP_API_URL}/createOpportunity`,
-//       {
-//         method: "post",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(opportunityData),
-//       }
-//     );
-//     return response.json();
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (
-//       opportunityData.title &&
-//       opportunityData.location &&
-//       opportunityData.organisation &&
-//       opportunityData.description &&
-//       opportunityData.objectives &&
-//       opportunityData.start_date &&
-//       opportunityData.close_date &&
-//       opportunityData.amount &&
-//       opportunityData.opp_type
-//     ) {
-//       postData().then((response) => {
-//         console.log(response);
-//         history.push(`/opportunity/${response.id}`);
-//       });
-//     }
-//   };
-
-//   return (
-//     <form>
-//       <div className="form-wrap">
-//         <label htmlFor="title">Opportunity Title</label>
-//         <input type="text" id="title" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="location">Location</label>
-//         <input type="text" id="location" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="organisation">Organisation</label>
-//         <input type="text" id="organisation" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="description">Description</label>
-//         <input type="text" id="description" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="objectives">Objectives</label>
-//         <input type="text" id="objectives" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="image">Image</label>
-//         <input type="text" id="image" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="start_date">Start Date</label>
-//         <input type="date" id="start_date" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="close_date">Close Date</label>
-//         <input type="date" id="close_date" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="amount">Amount</label>
-//         <input type="number" id="amount" onChange={handleChange} />
-//       </div>
-//       <div className="form-wrap">
-//         <label htmlFor="opp_type">Opportunity Type</label>
-//         <input type="text" id="opp_type" onChange={handleChange} />
-//       </div>
-
-//       <button type="submit" onClick={handleSubmit}>
-//         Create Opportunity
-//       </button>
-//     </form>
-//   );
-// }
-
-// export default NewOpportunityForm;
-
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import ButtonForms from "../../components/Button/ButtonForms";
@@ -260,7 +145,15 @@ function NewOpportunityForm() {
         id="location"
         type="text"
         label="Opportunity Location"
-        placeholder="Where is your opportunity located?!"
+        placeholder="Where is your opportunity located?"
+        onChange={handleChange}
+        error={errorMessages.title}
+      />
+      <TextInput
+        id="organisation"
+        type="text"
+        label="Organisation"
+        placeholder="Name of the organisation"
         onChange={handleChange}
         error={errorMessages.title}
       />
