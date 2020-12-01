d94816cb794ceb5f1210ae5d5ba9974f77b6c117
gianninapg@gmail.com <Gi@090488>
2020-12-01 20:25:55 +1000

    updated addFav.jsx

--- src/pages/NewOpportunityPage.jsx
+++ src/pages/NewOpportunityPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import NewOpportunityForm from "../components/NewOpportunityForm/NewOpportunityForm";
 import Header from "../components/Header/Header";
-import "./OpportunityPage.css"
 
 function NewOpportunityPage() {
   return (
--- src/pages/OpportunityPage.jsx
+++ src/pages/OpportunityPage.jsx
@@ -8,6 +8,8 @@ function OpportunityPage() {
     const [opportunityData, setOpportunityData] = useState([]);
     const { id } = useParams();
 
+    let opportunityId = window.localStorage.getItem("opportunity_id").toString();
+
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/opportunity/${id}/`).then((results) => {
             return results.json();
@@ -16,14 +18,22 @@ function OpportunityPage() {
         });
     }, []);
 
+    const postFavouriteData = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/favourites/`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({id:{opportunityId}})
+        });
+        return response.json();};
+
     return (
         <div>
             <Header />
                 <div className="opportunityPage">
-                    <div className="title">
-                        <h1>{opportunityData.title}</h1>
-                    </div>
                     <div className="column-1">
+                        <h1>{opportunityData.title}</h1>
                         <img src={opportunityData.image} />
                         <h3>Created at: {convertDateTime(opportunityData.date_created,0)}</h3>
                         <h3>Location: {opportunityData.location}</h3>
@@ -34,7 +44,10 @@ function OpportunityPage() {
                     </div>
                     <div className="column-2">
                         <h3>Opportunity Objectives:{opportunityData.objectives}</h3>
-                    </div>          
+                    </div>
+                    <div>
+                        <button type="submit" onClick={postFavouriteData}>Click</button>
+                     </div>          
                 </div>
         </div>
     );
