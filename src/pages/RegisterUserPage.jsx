955f95a3ba3186cd6be0ea44dfb55257504e067d
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-12-01 20:24:33 +1000

    more changes to forms

--- src/pages/RegisterUserPage.jsx
+++ src/pages/RegisterUserPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import RegisterUserForm from "../components/RegisterUser/RegisterUserForm";
 import Header from "../components/Header/Header";
+import "/Users/kritibhardwaj/Desktop/SheCodesBrisbane2020/FinalProject/plus-brisbane-2020-popping-pink-pythons-react/src/index.css";
 
 function RegisterUserPage() {
-  return <RegisterUserForm />;
+  return (
+    <div>
+      <Header />
+      <RegisterUserForm />
+    </div>
+  );
 }
 
 export default RegisterUserPage;
