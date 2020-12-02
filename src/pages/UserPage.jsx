72f8eb6fc95a670b75f2f6092aac0e9bd730c3e2
Natasha Seeto <tashseeto@gmail.com>
2020-12-03 03:47:38 +1000

    updated userpage css

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -33,36 +33,71 @@ function UserPage() {
         )
     } else {
         return (
-            <div className="outer-container">
+            <div>
                 <Header />
-
-                <div className="profile-container">
-                    <div className="profile-top-container">
-                        <div id="profile-img-container">
-                            <img alt="" className="profile-img" src={userData.image} />
-                        </div>
-                        <div id="profile-info-container">
-                            <h2>{userData.username}</h2>
-                            <h3>Joined on {convertDateTime(userData.date_created,0)}</h3>
-                        </div>
-                        <div id="profile-faves-container">
-                            <h2>{userData.num_fav}</h2>
+                    <div>
+                        <div className="grid-container">
+                            <div className="profile-grid-1">
+                                <h1>Profile</h1>
+                            </div>
+                            <div className="profile-grid-2">
+                                <img alt="" className="profile-img" src={userData.image} />
+                            </div>
+                            <div className="profile-grid-3"> 
+                                <div className="inner-3">
+                                    <h2>{userData.username}</h2>
+                                </div>
+                                <div className="inner-3">
+                                <h2>Joined on {convertDateTime(userData.date_created,0)}</h2>
+                                </div>
+                            </div>
+                            <div className="profile-grid-4">
+                                <div className="inner-4">
+                                    <h1>1{userData.num_fav}</h1>
+                                    <h3>favourite</h3>
+                                </div>
+                                <div className="inner-4">
+                                    <h1>125</h1>
+                                    <h3>interests</h3>
+                                </div>
+                                <div className="inner-4">
+                                    <h1>4</h1>
+                                    <h3>connections</h3>
+                                </div>
+                            </div>
+                            <div className="profile-grid-5">
+                                <div className="inner-5">
+                                    <h2>Bio</h2>
+                                    <p>{userData.bio}</p>
+                                </div>
+                            </div>
+                            <div className="profile-grid-6">
+                                <h1>Favourites</h1>
+                            </div>
+                            <div className="profile-grid-7">
+                                <div className="inner-fave">
+                                    <p>Favourite #1 - data to be pulled through as card</p>
+                                </div>
+                            </div>
+                            <div className="profile-grid-8">
+                                <div className="inner-fave">
+                                    <p>Favourite #2 - data to be pulled through as card</p>
+                                </div>
+                            </div>
+                            <div className="profile-grid-9">
+                                <div className="inner-fave">
+                                <   p>Favourite #3 - data to be pulled through as card</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="profile-bottom-container">
-                        <div id="bio-container">
-                            <h2>Bio</h2>
-                            <p>{userData.bio}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="favourites-container">
-                    <p>this is where the faves go :)</p>
-               </div>
-            </div>
+            </div>            
         );
     }
 }
 
+
+                        {/* <div className="favourites-container">
+                            <p>this is where th faves go!</p>
+                        </div> */}
 export default UserPage;
