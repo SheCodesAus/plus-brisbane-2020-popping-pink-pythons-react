f4ce447e432897ea40ff95b61a6843bfdeb33e4e
Natasha Seeto <tashseeto@gmail.com>
2020-12-04 16:31:36 +1000

    finalised userpage css

--- src/components/FaveCard/FaveCard.jsx
+++ src/components/FaveCard/FaveCard.jsx
@@ -1,40 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import './FaveCard.css';
 import convertDateTime from '../helpers/DateConverter';
+import useModal from '../helpers/useModal';
+import Modal from '../helpers/Modal';
 
-function FaveCard() {
-    const [userData, setUserData] = useState({ favourites: [] });
- 
-    useEffect(() => {
-        let token = window.localStorage.getItem("token");
-        fetch(`${process.env.REACT_APP_API_URL}/users/currentuser`,{
-            method: "get",
-            headers: {
-            'Authorization': `Token ${token}`
-        }})
-        .then((results) => {
-            return results.json();
-        }).then((data) => {
-            setUserData(data);
-            console.log(data)
-        });
-    }, []);
+
+function FaveCard(props) {
+    const { favouritesData } = props;
+    const { isShowing, toggle } = useModal();
+    const [favouritesId, setFavouritesId] = useState(0);
+    let newFavouritesId = 0;
+
+
+    const changeId = () => {
+        newFavouritesId = window.localStorage.setItem("favourites_id", favouritesData.id);
+        setFavouritesId(newFavouritesId);
+    }
+
+    const handleClick = () => {
+        console.log(favouritesData.id)
+        changeId();
+        toggle();
+    }
 
     return (
-            <div className="fave-card">
-                {userData.favourites.map((favouritesData, key) => {
-                    return (
-                        <div>
-                            <img src={favouritesData.image} />
-                            <h3>{favouritesData.title}</h3>
-                            <h4>Closes on {convertDateTime(favouritesData.close_date,0)}</h4>
-                            <h4>{favouritesData.opp_type} Amount: ${favouritesData.amount}</h4>
-                        </div>
-                    );
-                })}
-            </div>
+        <div className="fave-card-container">
+            <button className="fave-button" onClick={handleClick}>
+                <img src={favouritesData.image} />
+                <h3>{favouritesData.title}</h3>
+                <h4>Closes on {convertDateTime(favouritesData.close_date,0)}</h4>
+                <h4>{favouritesData.opp_type} Amount: $ {favouritesData.amount}</h4>
+            </button>
+            <Modal
+                isShowing={isShowing}
+                hide={toggle}
+                favouritesId = {favouritesId}
+            />
+        </div>
     );
 }
 
 export default FaveCard;
 
+
+    // const [userData, setUserData] = useState({ favourites: [] });
+
+ 
+    // useEffect(() => {
+    //     let token = window.localStorage.getItem("token");
+    //     fetch(`${process.env.REACT_APP_API_URL}/users/currentuser`,{
+    //         method: "get",
+    //         headers: {
+    //         'Authorization': `Token ${token}`
+    //     }})
+    //     .then((results) => {
+    //         return results.json();
+    //     }).then((data) => {
+    //         setUserData(data);
+    //         console.log(data)
+    //     });
+    // }, []);
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import convertDateTime from "../components/helpers/DateConverter";
-import "./UserPage.css"
+import "./UserPage.css";
 import Header from "../components/Header/Header";
 import FaveCard from "../components/FaveCard/FaveCard";
 
+
 function UserPage() {
-    const [userData, setUserData] = useState( [] );
+    const [userData, setUserData] = useState({ favourites: [] } );
     const [noUser, setNoUser] = useState(false)
 
  
@@ -35,82 +36,72 @@ function UserPage() {
         return (
             <div>
                 <Header />
-                    <div>
-                        <div className="grid-container">
-                            <div className="grid a">
-                                   <p>header</p> 
-                            </div>
 
-                            <div className="grid b">
-                                <div className="grid-profile">
-                                    <div className="grid c">
-                                        <img alt="" className="profile-img" src={userData.image} />
-                                    </div>
-                                    <div className="grid d"> 
-                                        <div className="grid e">
-                                            <h1>{userData.username}</h1>
-                                        </div>
-                                        <div className="grid f">
-                                            <h3>Joined on {convertDateTime(userData.date_created,0)}</h3>
-                                        </div>
-                                    </div>
-                                    <div className="action"> 
-                                        <button className="btn hover">Edit
-                                        </button>
-                                    </div>
-                                    
-                                        <div className="grid g">
-                                            <div className="grid-stats hover g">
-                                                <div className="grid h">
-                                                    <h1>1{userData.num_fav}</h1>
-                                                    <h3>favourite</h3>
-                                                </div>
-                                                <div className="grid-stats hover i">
-                                                    <h1>125</h1>
-                                                    <h3>interests</h3>
-                                                </div>
-                                                <div className="grid-stats hover j">
-                                                    <h1>4</h1>
-                                                    <h3>connections</h3>
-                                                </div>
-                                            </div>
-                                        </div>                                    
-                                    </div>
-                            </div>
+                <div className="grid-container">
+                    <div className="grid-header">
+                    </div>
 
-                            <div className="grid k">
-                                <div className="grid l">
-                                    <h1>Profile</h1>
+                    <div className="profile-container">
+                        <div className="grid-profile">
+                            <div className="grid-pic">
+                                <img alt="" className="profile-img" src={userData.image} />
+                            </div>
+                            <div className="grid-name-container"> 
+                                <div className="e">
+                                    <h1>{userData.username}</h1>
                                 </div>
-                                <div className="grid m">
-                                    <p>{userData.bio}</p>
+                                <div className="f">
+                                    <h3>Joined on {convertDateTime(userData.date_created,0)}</h3>
                                 </div>
                             </div>
+                            <div className="action"> 
+                                <button className="btn hover">Edit
+                                </button>
+                            </div>
+                            <div className="grid g">
+                                <div className="grid-stats hover g">
+                                    <div className="grid h">
+                                        <h1>{userData.num_fav}</h1>
+                                        <h3>favourites</h3>
+                                    </div>
+                                    <div className="grid i">
+                                        <h1>125</h1>
+                                        <h3>interests</h3>
+                                    </div>
+                                    <div className="grid j">
+                                        <h1>4</h1>
+                                        <h3>connections</h3>
+                                    </div>
+                                </div>
+                            </div>                                    
+                        </div>
+                    </div>
+                            
+                    <div className="grid-bio">
+                        <div className="grid l">
+                            <h1>Profile</h1>
+                        </div>
+                        <div className="grid m">
+                            <h4>{userData.bio}</h4>
+                        </div>
+                    </div>
 
-                            <div className="grid n">
+                    <div className="favourites-container">
+                            <div className="grid-favourites">
                                 <div className="grid o">
                                     <h1>Favourites</h1>
                                 </div>
                                 <div className="grid p">
-                                    < FaveCard />
-                                </div>
-                                <div className="grid q">
-                                    < FaveCard />
-                                </div>
-                                <div className="grid r">
-                                    <p> </p>
+                                    {userData.favourites.map((favouritesData, key) => {
+                                    return <FaveCard key={key} favouritesData = {favouritesData} />;
+                                    })}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-           
         );
     }
 }
 
-
-                        {/* <div className="favourites-container">
-                            <p>this is where th faves go!</p>
-                        </div> */}
 export default UserPage;
